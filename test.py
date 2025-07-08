015953b32fbbbd7dd0487b79270a7209f2d18177
Tiger3422 <aidenalderfer342@gmail.com>
2025-07-07 21:42:04 -0400

    first commit

---
+++ test.py
@@ -0,0 +1,21 @@
+import requests
+
+base_url = 'https://api-v3.mbta.com/'
+
+payload = {'filter[route]': 'Orange'}
+
+# Function to get stop information by stop ID
+
+def get_stop_info(stop_id):
+    r = requests.get(f'https://api-v3.mbta.com/stops/{stop_id}')
+    
+    if r.status_code == 200:
+        return r.json() 
+    else: 
+        print(f"Failed to retrieve data: {r.status_code}")
+
+stop_id= "70025"
+stop_info=get_stop_info(stop_id)
+
+if stop_info:
+    print(f"Name: {stop_info['data']['attributes']['name']}")
