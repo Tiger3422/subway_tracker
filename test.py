f62382140d7867628f2fe413e1df07aa63bf94a5
Tiger3422 <aidenalderfer342@gmail.com>
2025-07-08 23:16:10 -0400

    added stop by route function

--- test.py
+++ test.py
@@ -1,10 +1,23 @@
 import requests
+import json
 
 base_url = 'https://api-v3.mbta.com/'
 
 payload = {'filter[route]': 'Orange'}
 
 # Function to get stop information by stop ID
+
+
+def get_stops_by_route(route):
+    payload = {'filter[route]' : route}
+    r = requests.get(f'{base_url}/stops', params=payload)
+    data = r.json()
+    print(json.dumps(data, indent=2))  # Pretty print
+    return data
+    
+    
+get_stops_by_route("Orange")
+    
 
 def get_stop_info(stop_id):
     r = requests.get(f'https://api-v3.mbta.com/stops/{stop_id}')
@@ -17,5 +30,5 @@ def get_stop_info(stop_id):
 stop_id= "70025"
 stop_info=get_stop_info(stop_id)
 
-if stop_info:
-    print(f"Name: {stop_info['data']['attributes']['name']}")
+#if stop_info:
+#    print(f"Name: {stop_info['data']['attributes']['name']}")
