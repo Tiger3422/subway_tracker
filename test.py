4dc04119238f0198bff87013f8b6f1fb8af4831d
Tiger3422 <aidenalderfer342@gmail.com>
2025-07-18 23:29:26 -0400

    light_logic

--- test.py
+++ test.py
@@ -1,12 +1,15 @@
 import requests
 import json
+from datetime import datetime
 
 base_url = 'https://api-v3.mbta.com/'
 
 payload = {'filter[route]': 'Orange'}
 
 # Function to get stop information by stop ID
+import time
 
+current_time=time.time()
 
 def get_stops_by_route(route):
     payload = {'filter[route]' : route}
@@ -38,7 +41,7 @@ def get_stop_info(stop_id):
     else: 
         print(f"Failed to retrieve data: {r.status_code}")
 
-
+#obtain predictions 
 def get_stop_predictions(stop_id, route, direction):
     payload = {'filter[stop]' : stop_id, 'filter[route]' : route, 'filter[direction_id]' : direction}
     r = requests.get(f'{base_url}/predictions', params=payload)
@@ -52,12 +55,39 @@ stop_id= "place-haecl"
 stop_info=get_stop_predictions(stop_id, "Orange", 1)
 
 if 'data' in stop_info:
-    for i in range(min(3, len(stop_info['data']))):
+    #run through all train predictions
+    for i in range(len(stop_info['data'])):
+        #get stop info for the index prediction
         prediction = stop_info['data'][i]
-        print(prediction['attributes']['arrival_time'])
+        #convert the arrival time to python time
+        time_str = prediction['attributes']['arrival_time']
+        dt = datetime.fromisoformat(time_str)
+        unix_timestamp = int(dt.timestamp())
+        print(f" Train {prediction['relationships']['vehicle']['data']['id']} will arrive at {dt}" )
 
-#print(stop_info)
 
-with open("output.json", "w", encoding="utf-8") as f:
-    json.dump(stop_info, f, indent=2)
+def turn_on_led(led_number):
+    print(f"light at {station} is on")
+
+
+def light_logic(c_stop, vech_list):
+    prediction = stop_info['data'][i]
+    train_id = prediction['relationships']['vechile']['data']['id']
+    
+    #
+    if train_id in vech_list:
+        #get arrive time and convert it to unix
+        dt = datetime.fromisoformat(prediction['attributes']['arrival_time'])
+        arrive_time = int(dt.timestamp())
+        if(arrive_time-current_time<60):
+    
+    
+    
+    
+
+
+
+# with open("output.json", "w", encoding="utf-8") as f:
+#     json.dump(stop_info, f, indent=2)
+
 
